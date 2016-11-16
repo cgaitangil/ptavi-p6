452fd8af75079f21594373a0b4768e4af499ca3d
cgaitangil <cgaitangil@gmail.com>
2016-11-16 18:27:28 +0100

    ------

--- client.py
+++ client.py
@@ -15,30 +15,38 @@ import sys
 
 try:
     LINE = sys.argv[1:]
-    LINE = LINE[0].upper() + ' ' + LINE[1]
-    LIST = LINE.split(' ')
-
-    # Dirección IP del servidor.
-    SERVER = LIST[-1][LIST[-1].find('@') + 1:LIST[-1].find(':')]
-    PORT = int(LIST[-1][LIST[-1].find(':') + 1:])
-
+    
+    REC_IP = LINE[1][LINE[1].find('@') + 1:LINE[1].find(':')]
+    REC_PORT = int(LINE[1][LINE[-1].find(':') + 1:])
+    REC_NAME = LINE[1][:LINE[1].find('@')]
+  
+    LINE = LINE[0].upper() + ' sip:' + LINE[1][:-5] + ' SIP/2.0' + '\r\n\r\n'
+   
 except:
     Error = 'Usage: python3 client.py method receiver@IP:SIPport'
-    sys.exit('\n' + Error + '\n')
+    sys.exit(Error + '\n')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((REC_IP, REC_PORT))
 
-print('')
+print('\n' + "Sending: " + LINE)
+my_socket.send(bytes(LINE, 'utf-8'))
 
-print("Sending: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
+response = data.decode('utf-8')
 
-print('Received:' + '\n' + data.decode('utf-8'))
-print("Ending socket..." + '\n')
+print(response.split(' '))
+print('Received:' + '\n' + response)
+
+# Enviamos ACK si recibimos el paquete con las cabeceras Trying, Ring y OK.
+ack = 'ACK sip:' + REC_NAME + '@' + REC_IP + 'SIP/2.0'
+print(response.split(' ')[2][0:7])
+if response.split(' ')[2][0:7] == 'Trying':
+    my_socket.send(bytes(ack, 'utf-8'))
+
+print("Finished Socket." + '\n')
 
 # Cerramos todo
 my_socket.close()
